7287a5416ee44bd3b54037f15677ca93c90178e4
Anupam <9582anupamk@gmail.com>
2025-04-14 02:27:20 +0530

    added more language support

--- api/index.py
+++ api/index.py
@@ -15,11 +15,56 @@ def test_api():
 @app.route('/api/transcript/<video_id>', methods=['GET'])
 def get_transcript(video_id):
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(
-            video_id, 
-            languages=['hi'],  # Specify Hindi language
-            cookies='cookies.txt'  # Use cookies for authentication
-        )
+        # Define high priority languages (English variants + Hindi variants)
+        high_priority_languages = [
+            'en', 'en-US', 'en-GB', 'en-IN', 'en-CA', 'en-AU',  # English variants
+            'hi', 'hi-IN'  # Hindi variants
+        ]
+        
+        # Define comprehensive list of supported languages
+        supported_languages = [
+            'af', 'am', 'ar', 'as', 'az', 'bn', 'be', 'bs', 'bg', 'ca', 
+            'ceb', 'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da', 'dv', 'nl', 
+            'en', 'eo', 'et', 'eu', 'fa', 'fil', 'fi', 'fr', 'fy', 'ga', 
+            'gd', 'gl', 'de', 'el', 'gu', 'ha', 'iw', 'hi', 'hu', 'id', 
+            'it', 'ja', 'kn', 'kk', 'ko', 'ky', 'lv', 'lt', 'lb', 'mk', 
+            'ml', 'mt', 'mr', 'ne', 'no', 'or', 'pl', 'pt', 'pa', 'ro', 
+            'ru', 'sr', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'sw', 'sv', 
+            'tg', 'ta', 'te', 'th', 'tr', 'tk', 'uk', 'ur', 'uz', 'vi', 
+            'cy', 'yi', 'zh'
+        ]
+        
+        # Get list of available transcripts
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        selected_transcript = None
+        
+        # First try high priority languages
+        for lang in high_priority_languages:
+            try:
+                selected_transcript = transcript_list.find_transcript([lang])
+                break
+            except:
+                continue
+        
+        # If no high priority language found, try other supported languages
+        if not selected_transcript:
+            for lang in supported_languages:
+                try:
+                    selected_transcript = transcript_list.find_transcript([lang])
+                    break
+                except:
+                    continue
+        
+        # If still no match, get any available transcript
+        if not selected_transcript:
+            # Get the first available transcript
+            for transcript in transcript_list:
+                selected_transcript = transcript
+                break
+        
+        # Get the actual transcript
+        transcript = selected_transcript.fetch()
+        
         return jsonify({
             "success": True,
             "transcript": transcript
